57729c0b2d2329edc803c1c878ab4ad28b617829
mysterious-shailendr <shailendrshrivastav1234@gmail.com>
2021-12-25 00:04:33 +0530

    commit

---
+++ simplesocial/posts/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.conf import settings
+from django.urls import reverse
+
+# pip install misaka
+# If using virtual environment then first run below command
+# conda install libpython m2w64-toolchain -c msys2
+# pip install misaka
+import misaka
+
+from groups.models import Group
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name="posts",on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name="posts",null=True, blank=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse(
+            "posts:single",
+            kwargs={
+                "username": self.user.username,
+                "pk": self.pk
+            }
+        )
+
+    class Meta:
+        ordering = ["-created_at"] # '-' sign for displaying in descending order
+        unique_together = ["user", "message"]
